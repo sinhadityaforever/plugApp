0f5f36599408d42e61eb42c61fde0e3db7e044dc
Aditya Sinha <sinhadityaforever@gmail.com>
2022-04-20 23:08:49 +0530

    Minor changes

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,19 +3,12 @@ import { initializeApp } from 'firebase/app';
 const { getFirestore } = require('firebase/firestore');
 import { getStorage } from 'firebase/storage';
 import { getDatabase } from 'firebase/database';
+import { firebaseConfig } from './firebaseConfigData';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-	apiKey: 'AIzaSyCBwccanPlUrTq2Yev8xEi6bEa7yDL5Fj8',
-	authDomain: 'plugapp-a1ceb.firebaseapp.com',
-	projectId: 'plugapp-a1ceb',
-	storageBucket: 'plugapp-a1ceb.appspot.com',
-	messagingSenderId: '955257461424',
-	appId: '1:955257461424:web:cc85fd9c20ac80e8e6f975'
-};
 
 // Initialize Firebase
 let db;
